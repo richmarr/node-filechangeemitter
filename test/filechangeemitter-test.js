a60a94f3b34fa8d11edf4949677b6afebe4f8893
Barret Schloerke <schloerke@gmail.com>
2012-03-29 15:50:38 -0700

    have test file utilize new format option

--- test/filechangeemitter-test.js
+++ test/filechangeemitter-test.js
@@ -1,22 +1,22 @@
 
 var testCase = require('nodeunit').testCase,
-	FileChangeEmitter = require('../index').FileChangeEmitter,
+	FileChangeEmitter = require('../index'),
 	fs = require('fs');
 
 // This is run by nodeunit
 module.exports = testCase({
-	
+
 	// Make sure we're traversing the DOM as expected
 	testModify : function(test){
-	
+
 		var fw = new FileChangeEmitter(['./test/data']);
-		
+
 		fw.on('change',function(file){
 			test.ok(file);
 			test.done();
 			fw.close();
 		});
-		
+
 		setTimeout(function(){
 			fs.writeFile('./test/data/test-data.txt', "test data");
 		},500);
